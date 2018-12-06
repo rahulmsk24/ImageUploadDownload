8e674515ec50b255b6e528d8c0218dd11091c9fe
user <rahuls.maske91@gmail.com>
2018-12-06 12:19:20 +0530

    Second commit

---
+++ app/src/main/java/project/rahul/com/imageuploaddownload/MainActivity.java
@@ -0,0 +1,13 @@
+package project.rahul.com.imageuploaddownload;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
